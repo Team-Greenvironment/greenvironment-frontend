838487a9fc1fad619bc2118bf14f5e7f28072b54
Max <s_ehrlicherschmid18@stud.hwr-berlin.de>
2019-10-15 15:07:19 +0200

    added functionality to login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from 'src/app/models/login';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  login: Login 
+ 
+  constructor(private loginService: LoginService) {
+    this.login = {passwordHash: null, email: null};
   }
 
+  onClickSubmit(pEmail: string, pPasswordHash: string) {
+    console.log('email: ' + pEmail); 
+    this.login.email = pEmail
+    this.login.passwordHash = pPasswordHash 
+
+    this.loginService.login(this.login)
+  }
+  ngOnInit() {}
 }
+
--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {Http, URLSearchParams, Headers} from '@angular/http';
+import { Login } from '../../models/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  public login(login : Login) {
+ 
+    //let url = './graphql'
+    let url = 'https://greenvironment.net/graphql'
+ 
+    let headers = new Headers();
+    headers.set('Content-Type', 'application/json');
+ 
+    return this.http.post(url, this.buildJson(login)).subscribe(response => console.log(response.text()));
+  }
+
+  public buildJson(login: Login): any {
+    const body =  {query: `mutation($email: String, $pwHash: String) {
+      login(email: $email, passwordHash: $pwHash) {id}
+    }`, variables: {
+        email: login.email,
+        pwHash: login.passwordHash,
+      }};
+    return body;
+  }
 }
