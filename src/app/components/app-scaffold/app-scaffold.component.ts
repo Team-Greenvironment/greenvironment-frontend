ff1c05fe6e6fab54649ecc70cdf75eea323b916c
lampe_n1 <niklas.lampe@biotronik.com>
2019-10-15 12:55:08 +0200

    merge

---
+++ src/app/components/app-scaffold/app-scaffold.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scaffold',
+  templateUrl: './app-scaffold.component.html',
+  styleUrls: ['./app-scaffold.component.sass']
+})
+export class AppScaffoldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
