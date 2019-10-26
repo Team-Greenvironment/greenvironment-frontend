0d96caa5b08ee575e5488980690d8ad630f3ee40
Max <s_ehrlicherschmid18@stud.hwr-berlin.de>
2019-10-26 18:45:27 +0200

    added profile page, action and level list

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Http, URLSearchParams, Headers} from '@angular/http';
+import { User } from 'src/app/models/user';
+import { Actionlist } from 'src/app/models/actionlist';
+import { Levellist } from 'src/app/models/levellist';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +11,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  actionlist: Actionlist = new Actionlist();
+  levellist: Levellist = new Levellist();
+  user: User = new User()
+  id : string
+  profileNotFound : boolean = false;
+  constructor(private router: Router,private http: Http) { }
+  
 
   ngOnInit() {
+    this.id = this.router.url.substr(this.router.url.lastIndexOf("/") + 1);
+    //let url = './graphql'
+    let url = 'https://greenvironment.net/graphql'
+    let headers = new Headers();
+    headers.set('Content-Type', 'application/json');
+ 
+    return this.http.post(url, this.buildJson(this.id))
+      .subscribe(response => {
+        console.log(response.text());
+        this.updateUserInfo(response.json());
+      }
+      );
   }
 
+  public updateUserInfo(response : any){
+    if(response.data.getUser != null){
+      this.profileNotFound = false;
+      this.user.loggedIn = true;
+      this.user.userID = response.data.getUser.id;
+      this.user.username = response.data.getUser.name;
+      this.user.handle = response.data.getUser.handle;
+      this.user.points = response.data.getUser.points;
+      this.user.level = response.data.getUser.level;
+      this.user.friendIDs = response.data.getUser.friends;
+    } else{
+      this.profileNotFound = true;
+    }
+  }
+
+  public buildJson(id: string): any {
+    const body =  {query: `query($userId: ID) {
+      getUser(userId:$userId){
+        id
+        handle
+        name
+        profilePicture
+        points
+        level    
+        friendCount
+        friends{
+          id
+        }
+        posts{
+          content
+        }   
+      }
+    }`, variables: {
+        userId: this.id 
+      }};
+    return body;
+  }
 }
